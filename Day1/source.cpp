0f6ebaf5ab4a9873a5644bca740c1e7af02c7e18
derisan <derisan@naver.com>
2021-03-02 15:06:12 +0900

    STL Day1 - file save

---
+++ Day1/source.cpp
@@ -0,0 +1,17 @@
+// source.cpp
+
+// STL DAY-1
+// Build settings : x86, Release
+
+#include "save.h"
+
+int main()
+{
+	Save( "source.cpp" );
+	Save( "save.h" );
+
+	return 0;
+}
+
+// 추가 학습
+// file text mode, binary mode
